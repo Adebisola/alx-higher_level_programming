3b6e6bf61e2cf1ceb97542e59ce5bfdfcd0cca55
Adebisola <adeniyiadebisola@gmail.com>
2022-11-29 13:20:16 -0800

    correct pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     remainder = number % 10
 if remainder > 5:
-        print(F"Last digit of {number:d} is {remainder:d} and is greater than 5")
+    print(F"Last digit of {number:d} is {remainder:d} and is greater than 5")
 elif remainder == 0:
-        print(F"Last digit of {number:d} is {remainder:d} and is 0")
+    print(F"Last digit of {number:d} is {remainder:d} and is 0")
 else:
-        print(F"Last digit of {number:d} is {remainder:d} and is less than 6 and not 0")
-
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, remainder))
