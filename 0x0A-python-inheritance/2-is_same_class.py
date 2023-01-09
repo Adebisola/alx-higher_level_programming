514e899d4eb40237efa0c23db360fe9ed74751d0
Adebisola <adeniyiadebisola@gmail.com>
2023-01-09 12:33:24 -0800

    fix indentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" defines a function which checks instances of a class """
+"""Defines a function that checks if an object is an instance a class"""
 
 
 def is_same_class(obj, a_class):
-    """ function which checks if an object is the exact instances of a class """
+    """Checks if an object is exactly an instance of the specified class.
+    """
     if type(obj) is a_class:
         return True
     return False
