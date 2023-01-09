011a711277eedaa6f32877d0462100ef33ee7d4f
Adebisola <adeniyiadebisola@gmail.com>
2023-01-09 12:30:08 -0800

    fix pycodestyle

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-
 """ defines a function which checks instances of a class """
+
+
 def is_same_class(obj, a_class):
     """ function which checks if an object is the exact instances of a class """
     if type(obj) is a_class:
