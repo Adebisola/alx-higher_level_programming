04eb45d1d2702a226cd7a7ca4b6f7d10bc9026ff
Adebisola <adeniyiadebisola@gmail.com>
2023-03-14 11:15:29 -0700

    fix semistandard styling

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
- // a script that writes a class Rectangle that declares a rectangle
+// a script that writes a class Rectangle that declares a rectangle
 module.exports = class Rectangle {
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
