dfe69c2ff7301aa2a7d1a29330da4e674ee97ee0
Adebisola <adeniyiadebisola@gmail.com>
2023-03-14 15:19:39 -0700

    fix output fail

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Write a function that prints the number of arguments already printed and the new argument value.
+let count = 0;
 exports.logMe = function (item) {
-  let count = 0;
   console.log(`${count++}: ${item}`);
 };
