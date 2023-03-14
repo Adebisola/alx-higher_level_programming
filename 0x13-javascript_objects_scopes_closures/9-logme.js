4406950e37d95c012ed30f9b6414d3e2e18ec2fb
Adebisola <adeniyiadebisola@gmail.com>
2023-03-14 15:16:57 -0700

    fix semistandard styling

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
- // Write a function that prints the number of arguments already printed and the new argument value.
-exports.logMe = function(item) {
-    let count = 0;
-    console.log(`${count++}: ${item}`);
-
+// Write a function that prints the number of arguments already printed and the new argument value.
+exports.logMe = function (item) {
+  let count = 0;
+  console.log(`${count++}: ${item}`);
 };
