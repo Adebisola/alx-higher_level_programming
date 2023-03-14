7e8a0bf72d9ec75b7a60333f09ef9b9bc81607f5
Adebisola <adeniyiadebisola@gmail.com>
2023-03-14 14:27:49 -0700

    fix semistandard style, fix output fail

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function(list, searchElement) {
-    let count = 0;
-    for (let i = 0; i < list.length; i++) {
-        let num = list[i];
-        if (num === searchElement) {
-            count += 1;
-            console.log(count);
-        }
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    const num = list[i];
+    if (num === searchElement) {
+      count++;
     }
-}
+  }
+  return count;
+};
